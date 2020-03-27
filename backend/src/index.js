c22b137e547605650924cf0afa51c21e956e71a1
Denis Silva <denists88@gmail.com>
2020-03-27 18:34:54 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/**
+ * Tipos de Parametros;
+ * Query Params: Parâmetros nomeados enviados na rota apos "?" (filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc.
+ */
+
+ /**
+  * Driver: SELECT * FROM users
+  * Query BUilder: table('users').select('*').where()
+  */
+
+app.listen(4444, function(){
+    console.log("Servidor rodando na url http://localhost:4444");
+})
